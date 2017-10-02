306415facd829e53316d58c6765a12a00e8ceab0
elonder629 <elonder2016>
2017-10-02 07:52:05 -0400

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,39 @@
 package com.company;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Scanner input = new Scanner(System.in);
-        System.out.println("Please input a string to be encrypted");
-        String orgin = input.next();
-        StringBuilder encrypted = new StringBuilder();
-        Random rand = new Random();
-        int randomNum = (int)(Math.random()*(rand.nextInt((orgin.length() - 1) + 1) + 1));
-        System.out.println(randomNum);
-        for (int i = randomNum; i < orgin.length(); i++){
-            if(i == 0){
+        String word;
+        String encrypted = "";
+        System.out.println("Enter word");
+        word = input.nextLine();
+        int starter = (int)(Math.random() * word.length());
+        String beginner = word.substring(0, starter);
+        String ender = word.substring(starter);
+        word = ender + beginner;
 
-            }
-            else if(i == 1){
-
-            }
-
+        int order = (int)(Math.random() * 5);
+        for(int x = 1; x < word.length(); x++) {
+            char convert = word.charAt(x);
+            if(order  == 5)
+                order = 0;
+            else if(order == 0)
+                encrypted += Integer.toBinaryString((int)convert);
+            else if(order == 1)
+                encrypted += Integer.toOctalString((int)convert);
+            else if(order == 2)
+                encrypted += (int)convert;
+            else if(order == 3)
+                encrypted += Integer.toHexString((int)convert);
+            else if(order == 4)
+                encrypted += convert;
+            order++;
 
         }
+        System.out.println(encrypted);
 
     }
 }
